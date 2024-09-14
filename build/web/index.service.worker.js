e3028df12cfbcd9da3b2824275920373c7e34eba
pak <paklong2556@gmail.com>
2024-09-14 10:51:11 -0700

    update to 4.3

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1723526449|2439534123';
+const CACHE_VERSION = '1726336104|377419390';
 /** @type {string} */
 const CACHE_PREFIX = 'Queens-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

67ff297d89fb0b5fbc52c2d75561c0403f1c35a6
pak <paklong2556@gmail.com>
2024-09-14 11:18:02 -0700

    Removed Runnable option

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1726336104|377419390';
+const CACHE_VERSION = '1726337838|2111535307';
 /** @type {string} */
 const CACHE_PREFIX = 'Queens-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
